b566d201df78903d5306410cfb5c001d356d4d37
Dan Gohman <dev@sunfishcode.online>
2021-01-11 14:56:07 -0800

    Update build.rs.

--- build.rs
+++ build.rs
@@ -11,12 +11,19 @@ fn main() {
             "write_all_vectored", // https://github.com/rust-lang/rust/issues/70436
             "windows_by_handle",  // https://github.com/rust-lang/rust/issues/63010
             "windows_file_type_ext",
-            "try_reserve", // https://github.com/rust-lang/rust/issues/56431
-            "shrink_to",   // https://github.com/rust-lang/rust/issues/56431
-            "pattern",     // https://github.com/rust-lang/rust/issues/27721
-            "clamp",       // https://github.com/rust-lang/rust/issues/44095
+            "open_options_ext_as_flags", // https://github.com/rust-lang/rust/issues/76801
+            "try_reserve",               // https://github.com/rust-lang/rust/issues/56431
+            "shrink_to",                 // https://github.com/rust-lang/rust/issues/56431
+            "pattern",                   // https://github.com/rust-lang/rust/issues/27721
+            "clamp",                     // https://github.com/rust-lang/rust/issues/44095
+            "extend_one",                // https://github.com/rust-lang/rust/issues/72631
+            "toowned_clone_into",        // https://github.com/rust-lang/rust/issues/41263
         ] {
             println!("cargo:rustc-cfg={}", feature);
         }
     }
+
+    // Don't rerun this on changes other than build.rs, as we only depend on
+    // the rustc version.
+    println!("cargo:rerun-if-changed=build.rs");
 }
