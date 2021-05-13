641050a83810a8f2abaaf7d32043c62c3994dc5a
Dan Gohman <dev@sunfishcode.online>
2021-05-13 09:27:27 -0700

    Add a Stream-Safe NFC cat example.

---
+++ examples/ssnfc-cat.rs
@@ -0,0 +1,21 @@
+use std::io::{BufRead, Write};
+use unicode_normalization::UnicodeNormalization;
+
+fn main() -> anyhow::Result<()> {
+    let stdin = std::io::stdin();
+    let stdout = std::io::stdout();
+    let lock = stdin.lock();
+    let mut out = stdout.lock();
+    for line in lock.lines() {
+        let mut line = line?;
+        line.push('\n');
+        out.write_all(
+            line.chars()
+                .stream_safe()
+                .nfc()
+                .collect::<String>()
+                .as_bytes(),
+        )?;
+    }
+    Ok(())
+}
