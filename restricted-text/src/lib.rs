8da7c2220af5466ae2034159082ea2af39a8fd04
Dan Gohman <dev@sunfishcode.online>
2021-05-18 16:06:21 -0700

    Remove the old code for restricted strings, as it isn't currently up to date.

--- restricted-text/src/lib.rs
+++ restricted-text/src/lib.rs
@@ -1,4 +1,8 @@
 //! Restricted Text strings and I/O streams
+//!
+//! Restricted Text is more a collection of ideas than an actual format. The
+//! first step is to combine them into a coherent format which could be
+//! implemented.
 
 #![deny(missing_docs)]
 #![cfg_attr(can_vector, feature(can_vector))]
@@ -6,21 +10,3 @@
 #![cfg_attr(try_reserve, feature(try_reserve))]
 #![cfg_attr(pattern, feature(pattern))]
 #![cfg_attr(shrink_to, feature(shrink_to))]
-
-//mod copy;
-//mod read_restricted;
-//mod restricted_duplexer;
-//mod restricted_input;
-//mod restricted_output;
-//mod restricted_reader;
-//mod restricted_string;
-//mod restricted_writer;
-//mod write_restricted;
-
-//pub use copy::{copy_restricted, copy_restricted_using_status};
-//pub use read_restricted::{default_read_exact_restricted, ReadRestricted, ReadRestrictedLayered};
-//pub use restricted_duplexer::RestrictedDuplexer;
-//pub use restricted_reader::RestrictedReader;
-//pub use restricted_string::{RestrictedStr, RestrictedString};
-//pub use restricted_writer::RestrictedWriter;
-//pub use write_restricted::{default_write_restricted, WriteRestricted};
